d0764f2ca2284713f501628ee260d81f726325b7
Alejandro Coca <alxmcr@outlook.com>
2024-08-10 19:39:40 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# UI Pastelito
+
+It is an UI library components built on React.js
+
+## Installation
+
+```bash
+npm install @aledev-org/npm-002-ui-pastelito-jsx
+```
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: "MyReactComponents",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  externals: [nodeExternals()],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  mode: "production",
+};
