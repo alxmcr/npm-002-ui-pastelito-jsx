4838afc87bee4484dc24f1c529a2c51bc06b66fe
Alejandro Coca <alxmcr@outlook.com>
2024-08-10 23:27:27 -0400

    wip

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
   dts: true,
   sourcemap: true,
-  clean: true,
-  target: "esnext",
+  clean: true
 });
