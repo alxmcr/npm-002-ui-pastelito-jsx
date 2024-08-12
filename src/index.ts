6b75cf6b3999b6603d76c25eeb5ebedf556ec324
Alejandro Coca <alxmcr@outlook.com>
2024-08-12 02:11:27 -0400

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export { default as AppButton } from './components/AppButton';
-export { default as AppBox } from './components/AppBox';
+import "./index.css";
+
+export { default as AppButton } from "./components/AppButton";
+export { default as AppBox } from "./components/AppBox";
